c3c2879699d06bc6eb9a5f7a57f13e62efa60a57
Nick Nie <sky_elbor@hotmail.com>
2020-09-23 18:40:51 +1000

    Add project files.

---
+++ Api/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Models;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _storeContext;
+
+        public ProductsController(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            IEnumerable<Product> products = await _storeContext.Products.ToListAsync();
+            return Ok(products);
+        }
+    }
+}
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        { }
+    }
+}
